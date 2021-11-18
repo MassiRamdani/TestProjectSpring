69213ad523208d477a6b634049aaf7ca3fc611eb
massinissa06 <massinfoo@gmail.com>
2021-11-18 14:02:09 +0100

    my first

---
+++ ProjetTestSpringMicroService/src/main/java/com/project/m2dfs/ProjetTestSpringMicroService/ProjetTestSpringMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.m2dfs.ProjetTestSpringMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTestSpringMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetTestSpringMicroServiceApplication.class, args);
+	}
+
+}
---
+++ ProjetTestSpringMicroService/src/test/java/com/project/m2dfs/ProjetTestSpringMicroService/ProjetTestSpringMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.m2dfs.ProjetTestSpringMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetTestSpringMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
